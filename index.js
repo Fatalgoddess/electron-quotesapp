119bda8a5de07e827080b480f258fe994bb7cdd6
Antonia <antonia.ivanova09@yahoo.com>
2018-01-20 21:31:33 +0100

    jjh

--- index.js
+++ index.js
@@ -1,74 +1,39 @@
 const electron = require('electron');
 const{ app, BrowserWindow, Menu, ipcMain } = electron;
+let mainWindow;
 
 var Datastore = require('nedb'), db = new Datastore({filename : 'quotes'});
 db.loadDatabase();
 
-let mainWindow;
-
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({});
+  //TODO: Set icon property for OSX,
+  //for additional windows;
+  mainWindow = new BrowserWindow({icon:'images/app-icon.png'});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  //Load menu
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
 
-ipcMain.on('quote:add',(event, author,quote) => {
-   console.log("Even more things happened", author, quote);
-   if (author.value=="") { author.value = undefined;}
-   var doc = { author: author,
-               quote: quote,
-               day: new Date()
-               };
-   db.insert(doc, function (err, newDoc) {
-    console.log("successfully inserted in database");
-  });
-});
-
-ipcMain.on('quotes:getQuotes', (event) => {
-  console.log("IPC main get quotes");
-  db.find({}, function (err,docs){
-    console.log(docs);
-    mainWindow.webContents.send('quotes:receiveQuotes', (event, docs));
-  });
-})
-
+//Build Menu Template
 const menuTemplate = [
-  //On MacOS it merges with the first menu option,
-  //Must find other solution
-  {
-    label : 'File',
-    submenu : [
-    { label : 'New ToDo',
-      accelerator : process.plattform==='darwin' ? 'Command+N' : 'Ctrl+N',
-    },
-    {
-      label : 'Clear Todos',
-      accelerator : process.plattform==='darwin' ? 'Command+Shift+C' : 'Ctrl+Shift+C',
-      click() {
-        mainWindow.webContents.send('todo:clear');
+      {
+        label : 'Quit',
+        accelerator : process.plattform==='darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){app.quit();}
       }
-    },
-    { label : 'Quit',
-    accelerator : process.plattform==='darwin' ? 'Command+Q' : 'Ctrl+Q',
-    click(){
-      app.quit();
-    }
-  }
-]
-}
-];
+    ];
 
+//For MacOS
 if(process.plattform === 'darwin'){
-  //adds first arg to array
   menuTemplate.unshift({});
 }
 
+//Load Dev Tools option
 if(process.env.NODE_ENV !== 'production'){
   menuTemplate.push({
     label : 'Developer',
     submenu : [
-      //shortcut for menu item - predefined
       {role : 'reload'},
       {label : 'Toggle Developer Tools',
       accelerator : process.plattform==='darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
@@ -78,3 +43,37 @@ if(process.env.NODE_ENV !== 'production'){
     }]
   });
 }
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+//Add a quote
+ipcMain.on('quote:add',(event,author,quote) => {
+   //console.log("Even more things happened", author, quote);
+   if (author.value==""){
+     author.value = undefined;
+   }
+   var doc = {
+              author: author,
+              quote: quote,
+              day: new Date()
+            };
+   db.insert(doc, function (err, newDoc){
+    //console.log("Successfully inserted in database");
+  });
+});
+
+//Retrieve all quotes
+ipcMain.on('quotes:getQuotes',(event) => {
+  //console.log("IPC main get quotes");
+  db.find({}, function (err,docs){
+    //console.log(docs);
+    mainWindow.webContents.send('quotes:receiveQuotes',(event, docs));
+  });
+});
